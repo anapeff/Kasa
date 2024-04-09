d74ceb6734a8418f39a6df2bf19b5ff56af83fd2
anapeff <anais.pefferkorn@outlook.fr>
2024-04-09 10:41:00 +0200

    Modifications CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './_header.scss';
 import LogoKasa from '../../images/kasa.svg';
 
 function Header() {
   return (
-    <header>
-        <Link to="/"><img src={LogoKasa} alt='Kasa'/> </Link>
-      <nav className="nav">
-
-        <Link to="/"    className={({ isActive }) => isActive ? 'nav__link nav__link--active' : 'nav__link'
-          }
-          >Accueil</Link>
-
-
-        <Link to="/about"   className={({ isActive }) =>
-            isActive ? 'nav__link nav__link--active' : 'nav__link'
-          }
-          >A Propos
-          </Link>
+    <div className='header'>
+    <img className='header_logo' src={LogoKasa}/>
+    <nav className='header_link'>            
+        <NavLink exact="true" to="/" activeclassname="active">Accueil</NavLink> 
+        <NavLink to="/about" activeclassname="active">A propos</NavLink> 
       </nav>
-   
-    </header>
+</div>
   );
 }
 
